9856ec0cce7f554231db9b5b4c37e6ca2c861474
AlexVN <alexvn@MacBook-Pro-AlexVN.local>
2021-10-03 23:11:42 +0300

    new urls for categories

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import url
 from . import views
+from django.urls import path, include
 
 urlpatterns = [
-    url(r'^$', views.product_list, name='product_list'),
-    url(r'^(?P<category_slug>[-\w]+)/$',
-        views.product_list,
-        name='product_list_by_category'),
-    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
-        views.product_detail,
-        name='product_detail'),
+    path('', views.product_list, name='product_list'),
+    path('<category_slug>/', views.product_list, name='product_list_by_category'),
+    path('(?P<id>\d+)/(?P<slug>[-\w]+)/', views.product_detail, name='product_detail'),
 ]
+#
+#
+# urlpatterns = [
+#     path('index/', views.index, name='main-view'),
+#     path('bio/<username>/', views.bio, name='bio'),
+#     path('articles/<slug:title>/', views.article, name='article-detail'),
+#     path('articles/<slug:title>/<int:section>/', views.section, name='article-section'),
+#     path('weblog/', include('blog.urls')),
+#     ...
+# ]
